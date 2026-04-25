35555b5c9d280e4aa4764bb8346b24cc88fe0f53
toplutcapoo-hue <toplutcapoo@gmail.com>
2026-04-25 14:41:02 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
+    path('add/', views.add_expense, name='add_expense'),
 ]
