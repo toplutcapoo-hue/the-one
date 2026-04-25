c12879d3121bb9271b68e28d63c95380ccb4a3f5
toplutcapoo-hue <toplutcapoo@gmail.com>
2026-04-25 14:40:30 +0300

    Replace home view with index and add_expense functions
    Refactor home view to index and add expense handling.

--- core/views.py
+++ core/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Expense
+from .forms import ExpenseForm
 
-def home(request):
-    all_expenses = Expense.objects.all()
-    # Replace 'index.html' with your actual main file name
-    return render(request, 'index.html', {'expenses': all_expenses})
+def index(request):
+    expenses = Expense.objects.all().order_by('-date')
+    total = sum(exp.amount for exp in expenses)
+
+    return render(request, 'index.html', {
+        'expenses': expenses,
+        'total': total
+    })
+
+
+def add_expense(request):
+    if request.method == "POST":
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ExpenseForm()
+
+    return render(request, 'add.html', {'form': form})
